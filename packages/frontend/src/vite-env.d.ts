3a4fdb5b8e6a59a0a8cb712fb42153f6658ec343
nicholascannon <nicholascannon1@gmail.com>
2026-01-22 17:54:26 +1100

    lint:fix in fe

--- packages/frontend/src/vite-env.d.ts
+++ packages/frontend/src/vite-env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_CLERK_PUBLISHABLE_KEY: string;
 }
 
-// biome-ignore lint/correctness/noUnusedVariables: type override
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
