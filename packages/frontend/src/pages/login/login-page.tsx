a37b531f6a7557ce948833cf9538ac036aef9f7c
nicholascannon <nicholascannon1@gmail.com>
2026-01-23 19:02:58 +1100

    replace legacy styles

--- packages/frontend/src/pages/login/login-page.tsx
+++ packages/frontend/src/pages/login/login-page.tsx
@@ -2,8 +2,7 @@ import { SignIn } from '@clerk/clerk-react';
 
 export function LoginPage() {
   return (
-    // TODO: replace with tailwind
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div className='flex justify-center items-center h-screen'>
       <SignIn routing='path' path='/login' signUpUrl='/signup' />
     </div>
   );
--- packages/frontend/src/pages/signup/signup-page.tsx
+++ packages/frontend/src/pages/signup/signup-page.tsx
@@ -2,8 +2,7 @@ import { SignUp } from '@clerk/clerk-react';
 
 export function SignupPage() {
   return (
-    // TODO: replace with tailwind
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div className='flex justify-center items-center h-screen'>
       <SignUp routing='path' path='/signup' signInUrl='/login' />
     </div>
   );
