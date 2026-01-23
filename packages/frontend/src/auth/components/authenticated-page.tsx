cadaf34ab0d23c6dd1ebe35767a58dd89c86af80
nicholascannon <nicholascannon1@gmail.com>
2026-01-23 19:10:34 +1100

    Add loading spinner on auth page

--- packages/frontend/src/auth/components/authenticated-page.tsx
+++ packages/frontend/src/auth/components/authenticated-page.tsx
@@ -1,4 +1,5 @@
-import { SignedIn, SignedOut } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, useAuth } from '@clerk/clerk-react';
+import { Loader } from 'lucide-react';
 import { Navigate } from 'react-router';
 import { Page } from '@/components/page';
 
@@ -6,6 +7,16 @@ import { Page } from '@/components/page';
  * Wraps the children in a Page component and redirects to the login page if the user is not signed in.
  */
 export function AuthenticatedPage({ children, redirectUrl }: { children: React.ReactNode; redirectUrl?: string }) {
+  const { isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return (
+      <main className='flex justify-center items-center h-screen'>
+        <Loader className='animate-spin text-neutral-700' size={38} />
+      </main>
+    );
+  }
+
   return (
     <>
       <SignedIn>
