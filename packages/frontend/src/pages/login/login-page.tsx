ec8b1781c27993d645a95a734bad30c3c18cbf7f
nicholascannon <nicholascannon1@gmail.com>
2026-01-31 07:25:17 +1100

    Add loading indicator on clerk pages

--- packages/frontend/src/pages/login/login-page.tsx
+++ packages/frontend/src/pages/login/login-page.tsx
@@ -1,11 +1,19 @@
-import { SignIn } from '@clerk/clerk-react';
+import { SignIn, useSignIn } from '@clerk/clerk-react';
+import { Loader } from 'lucide-react';
 import { Page } from '@/components/page';
 
 export function LoginPage() {
+  const { isLoaded } = useSignIn();
   return (
     <Page>
       <div className='flex justify-center items-center mt-8'>
-        <SignIn routing='path' path='/login' signUpUrl='/signup' />
+        {isLoaded ? (
+          <SignIn routing='path' path='/login' signUpUrl='/signup' />
+        ) : (
+          <div className='flex justify-center items-center mt-8'>
+            <Loader className='animate-spin text-neutral-700' size={38} />
+          </div>
+        )}
       </div>
     </Page>
   );
--- packages/frontend/src/pages/signup/signup-page.tsx
+++ packages/frontend/src/pages/signup/signup-page.tsx
@@ -1,11 +1,20 @@
-import { SignUp } from '@clerk/clerk-react';
+import { SignUp, useSignUp } from '@clerk/clerk-react';
+import { Loader } from 'lucide-react';
 import { Page } from '@/components/page';
 
 export function SignupPage() {
+  const { isLoaded } = useSignUp();
+
   return (
     <Page>
       <div className='flex justify-center items-center mt-8'>
-        <SignUp routing='path' path='/signup' signInUrl='/login' />
+        {isLoaded ? (
+          <SignUp routing='path' path='/signup' signInUrl='/login' />
+        ) : (
+          <div className='flex justify-center items-center mt-8'>
+            <Loader className='animate-spin text-neutral-700' size={38} />
+          </div>
+        )}
       </div>
     </Page>
   );
